c10e062830a3d8f86daeb48e50e1c0c19deaee59
FG Ribreau <github@fgribreau.com>
2015-03-29 11:27:06 +0200

    refactor(app,config)

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+'use strict';
+
 /**
  * Redsmin proxy daemon
  * Francois-Guillaume Ribreau - @FGRibreau
  * redsmin.com
  */
 
-var config = require(__dirname+'/lib/config')();
-var Proxy  = require(__dirname+'/lib/Proxy');
+var fs = require('fs');
+var net = require('net');
+var tls = require('tls');
+
+var log = require('./lib/log')('debug');
+var config = require('./lib/config')(log);
+
+var jsonPackage = JSON.parse(fs.readFileSync('./package.json'));
+
+var Endpoint = require('./lib/Endpoint')(log, jsonPackage, tls, process);
+var RedisClient = require('./lib/RedisClient')(log, net);
+
+var Proxy = require(__dirname + '/lib/Proxy')(config, RedisClient, Endpoint);
 
 // Start the proxy
-new Proxy(config).start();
+new Proxy().start();
